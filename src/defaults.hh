5cd5c5a4eaf94561d58bb8c1b4bdf9039bfb9252
korei999 <ju7t1xe@gmail.com>
2024-11-23 02:16:00 +0200

    bump MAX_VOLUME to 1.5

--- src/defaults.hh
+++ src/defaults.hh
@@ -7,7 +7,7 @@ using namespace adt;
 namespace defaults
 {
 
-constexpr f32 MAX_VOLUME = 1.2f; /* [0.0f, 1.0f], > 1.0f will cause distortion */
+constexpr f32 MAX_VOLUME = 1.5f; /* [0.0f, 1.0f], > 1.0f may cause distortion */
 constexpr f32 VOLUME = 0.4f; /* startup volume */
 constexpr int UPDATE_RATE = 500; /* ui update rate, in ms */
 constexpr int READ_TIMEOUT = 5000; /* string input timeout, in ms */
