4cb276d4924e0c0f90cf712921c1dc7f8706578f
korei999 <ju7t1xe@gmail.com>
2025-09-23 13:15:54 +0300

    ignore return value

--- src/kmp3/platform/ansi/Win.cc
+++ src/kmp3/platform/ansi/Win.cc
@@ -128,7 +128,7 @@ void
 Win::wakeUp()
 {
     u64 t = 1;
-    (void)write(m_aFdsWakeUp[1], &t, sizeof(t));
+    std::ignore = write(m_aFdsWakeUp[1], &t, sizeof(t));
 }
 
 void
--- src/kmp3/platform/ansi/WinInput.cc
+++ src/kmp3/platform/ansi/WinInput.cc
@@ -412,7 +412,7 @@ Win::readFromStdin(const int timeoutMS)
     if (aPollFds[1].revents & POLLIN)
     {
         u64 t = 0;
-        (void)read(m_aFdsWakeUp[0], &t, 8);
+        std::ignore = read(m_aFdsWakeUp[0], &t, 8);
     }
 
     if (nRead == 0)
